bd2a021cee29f628da91940229df116f37a07e2e
shreejal maharjan <shreejal@rafled.com>
2021-08-05 20:19:37 +0545

    allow / and : :lock:

--- curl.php
+++ curl.php
@@ -8,6 +8,8 @@ if (isset($_POST['domain']))
     // disallow connection if domain has other than letters, numbers, and hyphens
     if (preg_match("/^[\/:.A-Za-z0-9-]+$/", $domain)) {
                 //execute shell command
+                //debug
+                echo $domain;
                 $shellexec = shell_exec("curl -I $domain");
                 echo '<pre>'.$shellexec.'</pre>';
     } else {
